70bd2ea104eb983c593a7db97c3f556e5fcf94b7
Ioana <mbuliga84@yahoo.com>
2026-03-07 19:40:26 +0200

    Adaugare tema

---
+++ Produs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temă_lab_2
+{
+    internal class Produs
+    {
+        public string nume;
+        public string categorie;
+        public double pret;
+        public int cantitate;
+
+        public void Afisare()
+        {
+            Console.WriteLine("Nume: " + nume);
+            Console.WriteLine("Categorie: " + categorie);
+            Console.WriteLine("Pret: " + pret);
+            Console.WriteLine("Cantitate: " + cantitate);
+        }
+    }
+}
