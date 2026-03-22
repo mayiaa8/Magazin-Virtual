b86a5ed49411e59d6b4ff126b8ec59681b3607c9
Ioana <mbuliga84@yahoo.com>
2026-03-22 12:25:25 +0200

    Temă laborator 4

---
+++ Temă laborator 4/NivelStocareDate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq; 
+using LibrarieModele;
+
+namespace NivelStocareDate
+{
+    public class AdministrareProduseMemorie
+    {
+        private List<Produs> produse;
+
+        public AdministrareProduseMemorie()
+        {
+            produse = new List<Produs>();
+        }
+
+        public void AddProdus(Produs p)
+        {
+            p.IdProdus = produse.Count + 1;
+            produse.Add(p);
+        }
+
+        public List<Produs> GetProduse()
+        {
+            return produse;
+        }
+
+        public Produs CautaProdusDupaNume(string nume)
+        {
+            
+            return produse.FirstOrDefault(p => p.Nume.Equals(nume, System.StringComparison.OrdinalIgnoreCase));
+        }
+
+        
+        public List<Produs> GetProduseDinCategorie(CategorieProdus cat)
+        {
+            return produse.Where(p => p.Categorie == cat).ToList();
+        }
+    }
+}
